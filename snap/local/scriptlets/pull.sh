654bbea85c1ebdd9afde4740dda5f6a4d153df3a
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2024-07-26 03:47:51 +0800

    snap: refactor: Install sbt using the new Snapcraft package repositories syntax
    Signed-off-by: 林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>

--- snap/local/scriptlets/pull.sh
+++ snap/local/scriptlets/pull.sh
@@ -27,56 +27,6 @@ if ! trap 'printf "Error: The program has encountered an unhandled error and is 
     exit 1
 fi
 
-# Download | sbt
-# https://www.scala-sbt.org/download
-printf \
-    'Info: Installing the sbt APT software sources list...\n'
-if ! echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" \
-    | tee /etc/apt/sources.list.d/sbt.list; then
-    printf \
-        'Error: Unable to install the sbt APT software sources list.\n' \
-        1>&2
-    exit 2
-fi
-
-printf \
-    'Info: Installing the sbt_old APT software sources list...\n'
-if ! echo "deb https://repo.scala-sbt.org/scalasbt/debian /" \
-    | tee /etc/apt/sources.list.d/sbt_old.list; then
-    printf \
-        'Error: Unable to install the sbt_old APT software sources list.\n' \
-        1>&2
-    exit 2
-fi
-
-printf \
-    'Info: Installing the sbt software sources signing public key...\n'
-if ! curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" \
-    | apt-key add; then
-    printf \
-        'Error: Unable to install the sbt software sources signing public key.\n' \
-        1>&2
-    exit 2
-fi
-
-printf \
-    'Info: Updating the APT software management system local cache...\n'
-if ! apt-get update; then
-    printf \
-        'Error: Unable to update the APT software management system local cache.\n' \
-        1>&2
-    exit 2
-fi
-
-printf \
-    'Info: Installing the sbt build tool...\n'
-if ! apt-get install -y sbt; then
-    printf \
-        'Error: Unable to install the sbt build tool.\n' \
-        1>&2
-    exit 2
-fi
-
 printf \
     'Info: Running the default logic of the pull step...\n'
 if ! craftctl default; then
